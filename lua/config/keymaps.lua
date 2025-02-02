d5e96b6f1f4edfb4987f689616834fff1563de0e
arion <tao.yang@bjyixu.com>
2025-02-03 00:07:50 +0800

    基本可用

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end)
+vim.keymap.set("n", "<F6>", function()
+  require("dap").step_over()
+end)
+vim.keymap.set("n", "<F7>", function()
+  require("dap").step_into()
+end)
+vim.keymap.set("n", "<F10>", function()
+  require("dap").step_out()
+end)
+vim.keymap.set("n", "<F9>", function()
+  require("dap").toggle_breakpoint()
+end)
